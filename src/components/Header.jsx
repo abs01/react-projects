f26c0e13bb0485c3a09964b7d9951488af622d8a
Antoni <Antoni>
2025-11-23 12:36:21 +0100

    cambio índices para netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ export default function Header({title}) {
             <h1>{title}</h1>
             <nav>
                 <a href="../../index.html">Pagina principal</a> &nbsp;
-                <a href="../../projectes.html">Projectes</a> &nbsp; 
-                <a href="../../newsLetter.html">Newsletter</a> &nbsp;
-                <a href="../../contacte.html">Contacte</a>   &nbsp;
+                <a href="../../indexes/projectes.html">Projectes</a> &nbsp; 
+                <a href="../../indexes/newsLetter.html">Newsletter</a> &nbsp;
+                <a href="../../indexes/contacte.html">Contacte</a>   &nbsp;
                 <button id="theme-button"><FaRegSun size={20}/></button>
             </nav>
             
