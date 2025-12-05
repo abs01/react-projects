99a63efb3c62246357fcd400c59fc13f7214b196
Antoni <Antoni>
2025-12-05 15:52:59 +0100

    intento leer json gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import './App.css'
 import './styles.css'
 import { AnimatedBackground } from 'animated-backgrounds';
 import Landing from './components/Landing'
+import Projectes from './components/Projectes';
 import Footer from './components/Footer';
 import Header from './components/Header';
 function App() {
 
   return (
     <>
-          <Header title="Principal"/>
-      <Landing />
-          <Footer />
+    <Header title="Proyectos" />
+    <Projectes />
+    <Footer />
 
     </>
   )
